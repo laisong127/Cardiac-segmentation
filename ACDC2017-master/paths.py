921e5c3a3fda8e217a1d0ed937bea28de77e6b36
laisong127 <1129542436@qq.com>
2020-09-07 17:06:35 +0800

    modify to run 2d

---
+++ ACDC2017-master/paths.py
@@ -0,0 +1,3 @@
+results_folder = "./result"  # set this to where you want to save the results
+path_acdc_3d = "/home/laisong/ACDC2017/3d_train"  # where to save preprocessed data for 3d network
+path_acdc_2d = "/home/laisong/ACDC2017/2d_train"  # where to save preprocessed data for 2d network
